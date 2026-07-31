15ce3f54052b034be83316eb68a8009dbe1eff7b
Fabio <fabioalmeida0896@gmail.com>
2026-07-31 14:51:55 -0300

    refactor: add comments for partner understand the flow of the webserv better

--- src/webserv.cpp
+++ src/webserv.cpp
@@ -3,34 +3,30 @@
 #include <iostream>
 #include <netdb.h>
 #include <sys/socket.h>
-// TEST: apenas para teste, o correto é preencher de acordo com o arquivo de
-// config
 
 WebServ::WebServ()
     : _port("8080"), _local_host("0.0.0.0"), _server_fd(-1), _opt(1),
       _backlog(128), _server_info(NULL), _getai_status(-1) {
-  // TODO:
-  // chamar a funcão do parser para preencher as portas aqui
-  // a classe parser teria que ter uma funcao que preenche
-  // as estruturas de acordo com o arquivo de config;
 }
 // WebServ WebServ(const WebServ &other);
 
 // NOTE: metodos privados de inicializacao do server;
 bool WebServ::setup_socket() {
+  //NOTE: limpa o struct do lixo de memoria, preenchendo ela com 0's
   std::memset(&_hints, 0, sizeof(_hints));
-  _hints.ai_family = AF_INET;
-  _hints.ai_socktype = SOCK_STREAM;
-  _hints.ai_flags = AI_PASSIVE;
+  _hints.ai_family = AF_INET; //familia de ip que vai ser usado AF_INET = IPV4
+  _hints.ai_socktype = SOCK_STREAM; // tipo do socket,  SOCK_STREAM = protocolo TCP
+  _hints.ai_flags = AI_PASSIVE; // IP CURINGA "0.0.0.0" 
+                                // que faz o kernel aceitar conexões em qualquer interface de rede disponivel
 
-  // NOTE: preencher a struct com as info importantes
+  // NOTE: preencher a struct server_info com as infos importantes configuradas no hints
   if ((_getai_status =
            getaddrinfo(NULL, _port.c_str(), &_hints, &_server_info)) != 0) {
     std::cerr << "getaddinfo : " << gai_strerror(_getai_status) << std::endl;
     return false;
   }
 
-  // NOTE: criacao do socket;
+  // NOTE: criacao do socket usando a struct que é preenchida por geataddrinfo;
   _server_fd = socket(_server_info->ai_family, _server_info->ai_socktype,
                       _server_info->ai_protocol);
   if (_server_fd < 0) {
@@ -52,6 +48,8 @@ bool WebServ::setup_socket() {
 }
 
 bool WebServ::setup_bind() {
+  //NOTE: bind serve para ligar o ip a uma porta(IP:PORT), aonde o socket vai escutar
+  //após o bind o servidor possui esse endereco
   if (bind(_server_fd, _server_info->ai_addr, _server_info->ai_addrlen) < 0) {
     perror("bind");
     cleanup_server();
@@ -62,6 +60,9 @@ bool WebServ::setup_bind() {
 }
 
 bool WebServ::setup_listen() {
+  //NOTE: listen serve para escutar no IP:PORTA e cada requisição é colocado numa fila.
+  //coloca o socket em modo de escuta, novas conexões TCP ficam na fila de espera até serem aceitas pelo
+  //accept
   if (listen(_server_fd, _backlog) < 0) {
     perror("listen");
     cleanup_socket();
@@ -86,6 +87,9 @@ bool WebServ::run() {
   if (!setup_server())
     return false;
 
+  //NOTE: loop principal do programa por enquanto atende a um cliente por vez;
+  //REFACTOR: usar o epoll depois para poder servir 
+  //mais de um cliente ao mesmo tempo
   while (1) {
     socklen_t _client_addr_size = sizeof(_client_addr);
 
@@ -103,6 +107,8 @@ bool WebServ::run() {
 
     char buffer[4096];
 
+    //NOTE: recv é bloqueante tbm, o kernel irá botar o processo em sleep
+    //até receber dados
     ssize_t bytes_rcv = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
 
     if (bytes_rcv < 0) {
@@ -127,6 +133,7 @@ bool WebServ::run() {
              "%s",
              strlen(body), body);
 
+    //NOTE:Envia a resposta HTTP ao cliente.
     send(client_fd, response, strlen(response), 0);
 
     close(client_fd);
